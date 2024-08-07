bc641ac074aabd15e2da86cbed7cd9edf538c371
pochemoo <boxofmonster@mail.ru>
2024-08-07 23:19:00 +0500

    fix bugs

--- scripts/animations.js
+++ scripts/animations.js
@@ -5,35 +5,35 @@ gsap.to('.header', {
 });
 
 gsap.to('.welcome__title', {
-    scrollTrigger: '.header',
+    scrollTrigger: '.welcome__title',
     opacity: 1,
     delay: 0.25,
     duration: 2.5,
 });
 
 gsap.to('.welcome__description', {
-    scrollTrigger: '.header',
+    scrollTrigger: '.welcome__description',
     opacity: 1,
     delay: 0.5,
     duration: 2.5,
 });
 
 gsap.to('.welcome__button', {
-    scrollTrigger: '.header',
+    scrollTrigger: '.welcome__button',
     opacity: 1,
     delay: 0.75,
     duration: 2.5,
 });
 
 gsap.to('.welcome__image', {
-    scrollTrigger: '.header',
+    scrollTrigger: '.welcome__image',
     opacity: 1,
     delay: 1,
     duration: 2.5,
 });
 
 gsap.to('.partners', {
-    scrollTrigger: '.header',
+    scrollTrigger: '.partners',
     opacity: 1,
     delay: 1.25,
     duration: 2.5,

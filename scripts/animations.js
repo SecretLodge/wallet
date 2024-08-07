82d045e977fce9a2943602ed71ad0515865899c9
pochemoo <boxofmonster@mail.ru>
2024-08-07 23:11:56 +0500

    add animations for two first sections

---
+++ scripts/animations.js
@@ -0,0 +1,46 @@
+gsap.to('.header', {
+    scrollTrigger: '.header',
+    opacity: 1,
+    duration: 2.5,
+});
+
+gsap.to('.welcome__title', {
+    scrollTrigger: '.header',
+    opacity: 1,
+    delay: 0.25,
+    duration: 2.5,
+});
+
+gsap.to('.welcome__description', {
+    scrollTrigger: '.header',
+    opacity: 1,
+    delay: 0.5,
+    duration: 2.5,
+});
+
+gsap.to('.welcome__button', {
+    scrollTrigger: '.header',
+    opacity: 1,
+    delay: 0.75,
+    duration: 2.5,
+});
+
+gsap.to('.welcome__image', {
+    scrollTrigger: '.header',
+    opacity: 1,
+    delay: 1,
+    duration: 2.5,
+});
+
+gsap.to('.partners', {
+    scrollTrigger: '.header',
+    opacity: 1,
+    delay: 1.25,
+    duration: 2.5,
+});
+
+gsap.to('.admin-panel__image', {
+    scrollTrigger: '.admin-panel__image',
+    opacity: 1,
+    duration: 2.5,
+});
